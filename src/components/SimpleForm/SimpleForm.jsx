22e44c76a504bfc55f0192ddb6f14547912ec71e
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-11 22:01:36 +0600

    handle form data collection with e.target input values

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -4,13 +4,15 @@ const SimpleForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('form submitted');
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder='your name'/>
+                <input type="text" name='name' placeholder='your name'/>
                 <br />
+                <input type="email" name='email' placeholder='your email' />
                 <input type="submit" value="Submit" />
             </form>
         </div>
