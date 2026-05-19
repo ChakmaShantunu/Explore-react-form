c2ba81de64ede647976b9de585b05066c62396f5
shantunu007 <chakmashantunu.web@gmail.com>
2026-05-19 21:39:36 +0600

    integrate multi-context and use() hook for state sharing

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,11 +1,14 @@
 import Grandpa from "./Grandpa";
 import "./FamilyTree.css"
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
-export const AssetContext = createContext();
+export const AssetContext = createContext('');
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
+
+    const [money, setMoney] = useState(0);
 
     const asset = 'diamond';
     const newAsset = 'Gold';
@@ -13,9 +16,12 @@ const FamilyTree = () => {
     return (
         <div className='family-tree'>
             <h2>Family Tree</h2>
-            <AssetContext.Provider value={newAsset}>
-                <Grandpa asset={asset}></Grandpa>
-            </AssetContext.Provider>
+            <h4>Total family money: {money}</h4>
+            <MoneyContext value={[money, setMoney]}>
+                <AssetContext.Provider value={newAsset}>
+                    <Grandpa asset={asset}></Grandpa>
+                </AssetContext.Provider>
+            </MoneyContext>
 
         </div>
     );
